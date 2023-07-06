e39e52bc9c25829328d1065516915c0a79579438
Mehrdad Kiani <mehrdad@evopricing.com>
2023-07-06 15:59:32 +0200

    Add new flow

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ## prefect --help
 ```
 prefect server start
-prefect agent start default-agent-pool
+prefect agent start --pool default-agent-pool --work-queue default
 
 prefect cloud login
 prefect cload logout
@@ -10,3 +10,17 @@ prefect profile ls
 prefect profile inspect default
 prefect profile use default
 ```
+
+Deployment configuration saved to prefect.yaml! You can now deploy using this deployment configuration with:
+
+        $ prefect deploy -n test_flow
+
+You can also make changes to this deployment configuration by making changes to the prefect.yaml file.
+
+To execute flow runs from this deployment, start a worker in a separate terminal that pulls work from the 'first-deploy-worker' work pool:
+
+        $ prefect worker start --pool 'first-deploy-worker'
+
+To schedule a run for this deployment, use the following command:
+
+        $ prefect deployment run 'test-flow/test_flow'
--- flowtest.py
+++ flowtest.py
@@ -1,12 +1,30 @@
 import httpx
-from prefect import flow
+from prefect import flow,task
 
+@task
+def fetch_weather_temperature():
+    res = httpx.get("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m")
+    return res.json()
+
+@task
+def fetch_weather_windspeed():
+    res = httpx.get("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=windspeed_10m")
+    return res.json()
+
+@task
+def fetch_weather_weathercode():
+    res = httpx.get("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m")
+    return res.json()
 
 @flow
-def test_flow():
-    res = httpx.get("https://example.com")
-    print(res)
+def main():
+    temp = fetch_weather_temperature()
+    wind = fetch_weather_windspeed()
+    weat = fetch_weather_weathercode()
 
+    print(temp)
+    print(wind)
+    print(weat)
 
 if __name__ == "__main__":
-    test_flow()
+    main()
