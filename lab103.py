b7016b9acac7ba6aa2ac51951bf4623d747a8c00
Mehrdad Kiani <mehrdad@evopricing.com>
2023-07-06 20:44:25 +0200

    Update lab103

--- lab103.py
+++ lab103.py
@@ -7,10 +7,15 @@ def who_is(user: str):
     logger.info("This task called by {}!".format(user))
     logger.info("Finishing task ...!")
 
+@flow
+def subflow_teset():
+    logger = get_run_logger()
+    logger.info("Running subflow ...!")
 
 @flow
 def main(default:str ='unknown'):
     who_is(default)
+    subflow_teset()
 
 
 if __name__ == "__main__":
